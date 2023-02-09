60c73942597a4faa151d8e22066b3628bb2211d9
Florian Heger <florian.heger@ruhr-uni-bochum.de>
2023-02-09 09:34:23 +0100

    initial commit

---
+++ src/main/java/de/fheger/floorplan2ifc/Floorplan2IfcApplication.java
@@ -0,0 +1,13 @@
+package de.fheger.floorplan2ifc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Floorplan2IfcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Floorplan2IfcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/fheger/floorplan2ifc/Floorplan2IfcApplicationTests.java
@@ -0,0 +1,13 @@
+package de.fheger.floorplan2ifc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Floorplan2IfcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
