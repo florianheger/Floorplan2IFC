47bc03d787d2ad2ff94c7df40ff7fd639b9543f3
fheger <fheger@brockhaus-ag.de>
2023-02-16 14:21:23 +0100

    changed relationship between doors and spaces to IfcRelSpaceBoundary

--- src/main/java/de/fheger/floorplan2ifc/logic/services/ifcservices/relationships/AddDoorRelationshipsService.java
+++ src/main/java/de/fheger/floorplan2ifc/logic/services/ifcservices/relationships/AddDoorRelationshipsService.java
@@ -6,12 +6,10 @@ import de.fheger.floorplan2ifc.logic.exceptions.ParseToIfcException;
 import de.fheger.floorplan2ifc.logic.services.FindIfcEntityService;
 import de.fheger.floorplan2ifc.models.entities.root.objectdefinition.object.product.element.builtelement.IfcDoor;
 import de.fheger.floorplan2ifc.models.entities.root.objectdefinition.object.product.spatialelement.spatialstructureelement.IfcSpace;
-import de.fheger.floorplan2ifc.models.entities.root.relationship.reldecomposes.IfcRelNests;
+import de.fheger.floorplan2ifc.models.entities.root.relationship.reldecomposes.IfcRelSpaceBoundary;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 
 @Service
@@ -30,12 +28,11 @@ public class AddDoorRelationshipsService implements AddRelationships<IfcDoor, Do
         if (connectedSpaces.size() == 0 || connectedSpaces.size() > 2) {
             throw new ParseToIfcException(ifcEntity.getName() + " connects 0 or more than 2 spaces.");
         }
-        List<IfcSpace> connectedIfcSpaces = new ArrayList<>();
         for (SpacePanel connectedSpace : connectedSpaces) {
-            connectedIfcSpaces.add(findIfcEntityService.findIfcEntity(ifcEntity, connectedSpace.getGlobalId(), IfcSpace.class));
+            IfcSpace connectedIfcSpaces = findIfcEntityService.findIfcEntity(ifcEntity, connectedSpace.getGlobalId(), IfcSpace.class);
+            IfcRelSpaceBoundary relDoorSpace = new IfcRelSpaceBoundary(connectedIfcSpaces, ifcEntity);
+            connectedIfcSpaces.getBoundedBy().add(relDoorSpace);
+            ifcEntity.getProvidesBoundaries().add(relDoorSpace);
         }
-        IfcRelNests relDoorSpaces = new IfcRelNests(ifcEntity, new HashSet<>(connectedIfcSpaces));
-        ifcEntity.getNests().add(relDoorSpaces);
-        connectedIfcSpaces.forEach(space -> space.getIsNestedBy().add(relDoorSpaces));
     }
 }
