a86b9e2e2c41c151a789e0490a5246e8793e0f3a
fheger <fheger@brockhaus-ag.de>
2023-02-21 15:40:28 +0100

    Added Slab

---
+++ src/main/java/de/fheger/floorplan2ifc/gui/nodes/SlabNode.java
@@ -0,0 +1,9 @@
+package de.fheger.floorplan2ifc.gui.nodes;
+
+import de.fheger.floorplan2ifc.gui.panels.placement.SlabPanel;
+
+public class SlabNode extends EntityNode<SlabPanel> {
+    public SlabNode() {
+        super(new SlabPanel(), "Slab.png");
+    }
+}
