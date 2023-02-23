5b10941c4d962187131ad388dc6f15ac3513790b
florianheger <florian.heger@outlook.de>
2023-02-23 12:47:04 +0100

    removed floor level

--- src/main/java/de/fheger/floorplan2ifc/gui/panels/placement/BuildingStoreyPanel.java
+++ src/main/java/de/fheger/floorplan2ifc/gui/panels/placement/BuildingStoreyPanel.java
@@ -1,16 +1,7 @@
 package de.fheger.floorplan2ifc.gui.panels.placement;
 
-import de.fheger.floorplan2ifc.gui.UiFactory;
-import de.fheger.floorplan2ifc.gui.inputs.IntegerNumberField;
-
 public class BuildingStoreyPanel extends EntityPanelWithPlacement {
-
-    private final IntegerNumberField floorNumber = UiFactory.createStandardIntegerNumberField();
-
     public BuildingStoreyPanel() {
         super("Storey", 0);
-
-        gridPane.add(UiFactory.createStandardLabel("Floor number:"), 0, ++rowsInEntityPanel);
-        gridPane.add(floorNumber, 1, rowsInEntityPanel);
     }
 }
