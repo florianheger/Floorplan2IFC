8426f1b85e8bab94ac7a8c60487c771287f1bfbb
fheger <fheger@brockhaus-ag.de>
2023-03-02 17:15:05 +0100

    specified parent of IfcNewObjectService

--- src/main/java/de/fheger/floorplan2ifc/logic/services/ifcservices/IfcNewObjectService.java
+++ src/main/java/de/fheger/floorplan2ifc/logic/services/ifcservices/IfcNewObjectService.java
@@ -1,4 +1,12 @@
 package de.fheger.floorplan2ifc.logic.services.ifcservices;
 
-public class IfcNewObjectService {
+import de.fheger.floorplan2ifc.interfaces.INewObject;
+import de.fheger.floorplan2ifc.logic.services.FindIfcEntityService;
+import de.fheger.floorplan2ifc.logic.services.ifcservices.attributes.IAddAttributesService;
+import de.fheger.floorplan2ifc.models.entities.root.objectdefinition.object.product.element.builtelement.IfcNewObject;
+
+public class IfcNewObjectService extends IfcService<IfcNewObject, INewObject> {
+    protected IfcNewObjectService(FindIfcEntityService findIfcEntityService, IAddAttributesService<IfcNewObject, INewObject> addAttributesService) {
+        super(IfcNewObject.class, INewObject.class, findIfcEntityService, addAttributesService);
+    }
 }
