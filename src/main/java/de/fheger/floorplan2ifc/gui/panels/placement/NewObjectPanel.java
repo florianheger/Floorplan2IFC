0ad5aa2213cb580ae7ec3c4b5d1b387adf284b69
fheger <fheger@brockhaus-ag.de>
2023-03-02 12:56:11 +0100

    added TextField; implemented INewObject

--- src/main/java/de/fheger/floorplan2ifc/gui/panels/placement/NewObjectPanel.java
+++ src/main/java/de/fheger/floorplan2ifc/gui/panels/placement/NewObjectPanel.java
@@ -1,7 +1,22 @@
 package de.fheger.floorplan2ifc.gui.panels.placement;
 
-public class NewObjectPanel extends PlacementPanel {
+import de.fheger.floorplan2ifc.gui.UiFactory;
+import de.fheger.floorplan2ifc.interfaces.INewObject;
+import javafx.scene.control.TextField;
+
+public class NewObjectPanel extends PlacementPanel implements INewObject {
+
+    private final TextField newAttributeField = UiFactory.createStandardTextField();
+
     public NewObjectPanel() {
         super("New Object", 0);
+
+        gridPane.add(UiFactory.createStandardLabel("New Attribute:"), 0, ++rowsInEntityPanel);
+        gridPane.add(newAttributeField, 1, rowsInEntityPanel);
+    }
+
+    @Override
+    public String getNewAttribute() {
+        return newAttributeField.getText();
     }
 }
