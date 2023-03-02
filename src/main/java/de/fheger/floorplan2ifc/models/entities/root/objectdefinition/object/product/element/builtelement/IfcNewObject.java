ddc5689c011a373c958aa1af38ddf1b0c8db27ef
fheger <fheger@brockhaus-ag.de>
2023-03-02 13:17:04 +0100

    added new attribute to IfcNewObject

--- src/main/java/de/fheger/floorplan2ifc/models/entities/root/objectdefinition/object/product/element/builtelement/IfcNewObject.java
+++ src/main/java/de/fheger/floorplan2ifc/models/entities/root/objectdefinition/object/product/element/builtelement/IfcNewObject.java
@@ -1,7 +1,9 @@
 package de.fheger.floorplan2ifc.models.entities.root.objectdefinition.object.product.element.builtelement;
 
 import de.fheger.floorplan2ifc.models.entities.root.objectdefinition.object.product.element.IfcBuiltElement;
+import lombok.Setter;
 
 public class IfcNewObject extends IfcBuiltElement {
-
+    @Setter
+    private String newAttribute;
 }
