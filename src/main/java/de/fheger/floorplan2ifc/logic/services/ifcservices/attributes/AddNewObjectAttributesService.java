7fe2e4f6159eead0754822cf3bd8671a206f70d7
fheger <fheger@brockhaus-ag.de>
2023-03-02 17:10:50 +0100

    implemented IAddAttributesService in AddNewObjectAttributesService

--- src/main/java/de/fheger/floorplan2ifc/logic/services/ifcservices/attributes/AddNewObjectAttributesService.java
+++ src/main/java/de/fheger/floorplan2ifc/logic/services/ifcservices/attributes/AddNewObjectAttributesService.java
@@ -1,4 +1,12 @@
 package de.fheger.floorplan2ifc.logic.services.ifcservices.attributes;
 
-public class AddNewObjectAttributesService {
+import de.fheger.floorplan2ifc.interfaces.INewObject;
+import de.fheger.floorplan2ifc.logic.exceptions.ParseToIfcException;
+import de.fheger.floorplan2ifc.models.entities.root.objectdefinition.object.product.element.builtelement.IfcNewObject;
+
+public class AddNewObjectAttributesService implements IAddAttributesService<IfcNewObject, INewObject> {
+    @Override
+    public void addAttributes(IfcNewObject ifcEntity, INewObject entityNode) throws ParseToIfcException {
+        ifcEntity.setNewAttribute(entityNode.getNewAttribute());
+    }
 }
