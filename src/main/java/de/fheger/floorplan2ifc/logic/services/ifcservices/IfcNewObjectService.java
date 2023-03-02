02e3f0a9bdc8eb9d52f8a692d16c09d820fa32f3
fheger <fheger@brockhaus-ag.de>
2023-03-02 17:16:05 +0100

    added Spring Dependency Injection annotations

--- src/main/java/de/fheger/floorplan2ifc/logic/services/ifcservices/IfcNewObjectService.java
+++ src/main/java/de/fheger/floorplan2ifc/logic/services/ifcservices/IfcNewObjectService.java
@@ -4,8 +4,12 @@ import de.fheger.floorplan2ifc.interfaces.INewObject;
 import de.fheger.floorplan2ifc.logic.services.FindIfcEntityService;
 import de.fheger.floorplan2ifc.logic.services.ifcservices.attributes.IAddAttributesService;
 import de.fheger.floorplan2ifc.models.entities.root.objectdefinition.object.product.element.builtelement.IfcNewObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class IfcNewObjectService extends IfcService<IfcNewObject, INewObject> {
+    @Autowired
     protected IfcNewObjectService(FindIfcEntityService findIfcEntityService, IAddAttributesService<IfcNewObject, INewObject> addAttributesService) {
         super(IfcNewObject.class, INewObject.class, findIfcEntityService, addAttributesService);
     }
--- src/main/java/de/fheger/floorplan2ifc/logic/services/ifcservices/attributes/AddNewObjectAttributesService.java
+++ src/main/java/de/fheger/floorplan2ifc/logic/services/ifcservices/attributes/AddNewObjectAttributesService.java
@@ -3,7 +3,9 @@ package de.fheger.floorplan2ifc.logic.services.ifcservices.attributes;
 import de.fheger.floorplan2ifc.interfaces.INewObject;
 import de.fheger.floorplan2ifc.logic.exceptions.ParseToIfcException;
 import de.fheger.floorplan2ifc.models.entities.root.objectdefinition.object.product.element.builtelement.IfcNewObject;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AddNewObjectAttributesService implements IAddAttributesService<IfcNewObject, INewObject> {
     @Override
     public void addAttributes(IfcNewObject ifcEntity, INewObject entityNode) throws ParseToIfcException {
